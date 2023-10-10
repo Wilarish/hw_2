4a3b8d80a28528b0f52d8cdf8a304bd6a4ed7185
Wilarish <tararammmm2004@gmail.com>
2023-10-10 17:12:51 +0400

    tests_6

--- src/routes/blogs-router.ts
+++ src/routes/blogs-router.ts
@@ -29,12 +29,12 @@ BlogsRouter.get('/:id', errorsChecking, async (req: Request<{ id: string }>, res
 })
 BlogsRouter.get('/:id/posts', errorsChecking, async (req: Request<{id: string}>, res:Response)=>{
 
-    // const blog = await blogsRepository.findBlogById(req.params.id)
-    // if(!blog) res.sendStatus(HTTP_statuses.NOT_FOUND_404)
+    const blog = await blogsRepository.findBlogById(req.params.id)
+    if(!blog) return  res.sendStatus(HTTP_statuses.NOT_FOUND_404)
 
     const pagination = getDefaultPagination(req.query)
     const posts =  await blogsRepository.findPostsForBlogsById(req.params.id, pagination)
-    res.status(HTTP_statuses.OK_200).send(posts)
+    return  res.status(HTTP_statuses.OK_200).send(posts)
 
 })
 BlogsRouter.post('/', authBasic, InputValidBlogs.post, errorsChecking, async (req: Request<{}, {}, { id: string, name: string, description: string, websiteUrl: string }>, res: Response) => {
