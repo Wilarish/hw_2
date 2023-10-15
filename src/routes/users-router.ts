5994c8c78542478d02a99eada0e65821f77fcbef
Wilarish <tararammmm2004@gmail.com>
2023-10-16 02:03:47 +0400

    hw_5_change_7_add_id_find

--- src/routes/users-router.ts
+++ src/routes/users-router.ts
@@ -9,6 +9,8 @@ import {usresServises} from "../domain/users-servises";
 import {InputValidationUsers} from "../middleware/arrays_of_input_validation";
 import {HTTP_statuses} from "../data/HTTP_statuses";
 import {UsersViewType} from "../types/users/users-view-type";
+import {PostsMainType} from "../types/posts/posts-main-type";
+import {postsRepository} from "../repositories/posts-rep";
 
 export const UsersRouter = Router({})
 
@@ -17,6 +19,16 @@ UsersRouter.get('/', authBasic, async (req: Request, res:Response) =>{
     const users:Paginated<UsersMainType> = await usersRepository.findUsers(pagination)
 
     res.status(HTTP_statuses.OK_200).send(users)
+})
+
+UsersRouter.get('/:id', authBasic, async (req:Request<{id:string}>, res:Response)=>{
+    const user: UsersViewType | null = await usersRepository.findUserById(req.params.id)
+
+    if (!user)
+        res.sendStatus(HTTP_statuses.NOT_FOUND_404)
+
+    else
+        res.send(user)
 })
 UsersRouter.post('/', authBasic,  InputValidationUsers.post, errorsChecking,  async (req:Request<{},{},{login: string, email: string, password: string},{}>, res:Response)=>{
     const data:UsersCreate = {
