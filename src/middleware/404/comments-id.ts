e25a6f65f6c90bd9e90ce286d2f17dce80de58d9
Wilarish <tararammmm2004@gmail.com>
2023-12-05 17:37:50 +0400

    hw_11_2_3

---
+++ src/middleware/404/comments-id.ts
@@ -0,0 +1,14 @@
+import {NextFunction, Request, Response} from "express";
+import {HTTP_STATUSES} from "../../data/HTTP_STATUSES";
+import {CommentsRepository} from "../../repositories/comments-rep";
+
+export const commentsId = async (req: Request<{ id: string }>, res: Response, next: NextFunction) => {
+
+    const commentRep = new CommentsRepository()
+
+    const comment = commentRep.findCommentById(req.params.id)
+
+    if (!comment) return res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
+    return next()
+
+}
