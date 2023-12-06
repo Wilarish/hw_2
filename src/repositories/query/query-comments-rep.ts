393e231e199f8ed71881b18efc71584f0c46a8fb
Wilarish <tararammmm2004@gmail.com>
2023-12-07 03:08:05 +0400

    hw_12_(((((_final_2

--- src/repositories/query/query-comments-rep.ts
+++ src/repositories/query/query-comments-rep.ts
@@ -3,21 +3,15 @@ import {CommentsViewType} from "../../types/comments-types";
 import {PostsMainType} from "../../types/posts-types";
 import {ObjectId} from "mongodb";
 import {CommentsModel} from "../../domain/models/models";
-import {LikeInfoView, LikesMainType, likeStatuses} from "../../types/likes-types";
+import {LikeInfoView} from "../../types/likes-types";
 import {PostsRepository} from "../posts-rep";
-import {CommentsServices} from "../../application/comments-services";
-import {LikesRepository} from "../likes-rep";
 import {RateHelp, RateHelpArr} from "../../helpers/rates-helper";
 
 export class QueryCommentsRepository {
     private postsRepository: PostsRepository;
-    private commentsServices: CommentsServices
-    private likesRepository: LikesRepository;
 
     constructor() {
         this.postsRepository = new PostsRepository()
-        this.commentsServices = new CommentsServices()
-        this.likesRepository = new LikesRepository()
     }
 
     async queryFindPaginatedComments(pagination: DefaultPaginationType, postId: string, userId: string|undefined): Promise<Paginated<CommentsViewType> | null> {
